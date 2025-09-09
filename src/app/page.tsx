21529829f7921e13041a061177a3178861de8e78
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-09-09 08:55:50 +0200

    Remove unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { Clue, DialogContent } from "@/util/types";
 
